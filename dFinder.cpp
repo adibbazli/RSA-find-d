f43ff2139c89730d04b77458ab2d8dc3fac1740c
Adib Bazli <35474407+adibbazli@users.noreply.github.com>
2018-07-26 16:34:32 +0800

    Update dFinder.cpp

--- dFinder.cpp
+++ dFinder.cpp
@@ -1,29 +1,33 @@
 #include<iostream>
-#include<math.h>
+#include<math.h> // to use fmod
+#include<cstdlib>
 using namespace std;
 // Written by Adib Bazli
 // You may reuse, distribute or resell it
 // This program brute force e*d mod fn = 1
 // This program does not come with warranty and I will not responsible with any damage may cause by this program
-// If you enter wrong number loop may occur, please do reset the program
+// If you enter wrong number infinite loop may occur, please do reset the program
 int main()
 {
     int d;
     float e, n, fn, p, q, sas = 0;
 
-    cout << "Copyright Adib Bazli 2018\nEnter p : ";
+    cout << "Find d for RSA by Adib Bazli, UTMKL 2018\n" << endl;
+
+    cout << " Enter prime number for p and q\n";
+    cout << " Enter p : ";
     cin >> p;
-    cout << "Enter q : ";
+    cout << " Enter q : ";
     cin >> q;
 
     fn = (p-1)*(q-1);
 
-    cout << "fn is " << fn << "\nEnter e : ";
+    cout << "\n fn is " << fn << endl << "\n Enter e : ";
     cin >> e;
 
     if(e >= fn )
     {
-        cout << "e can be bigger than fn";
+        cout << " e cant be bigger than fn";
         return 0;
     }
 
@@ -41,10 +45,14 @@ int main()
     }
 
     d = notD;
-    cout << "D is : " << d << endl;
-    cout << "\nn is : " << n << endl;
-    cout << "e is : " << e << endl;
-    cout << "fn is : " << fn << endl;
 
+    cout << "\n n is : " << n << endl;
+    cout << " fn is : " << fn << endl;
+    cout << " e is : " << e << endl;
+
+
+    cout << "\n d is : " << d << endl;
+
+    system("pause");
 
 }
